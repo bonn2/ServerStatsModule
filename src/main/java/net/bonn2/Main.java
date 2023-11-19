fcec339c27d852b74e43d0b7fd9fef03b57e5a24
Brett Bonifas <42011672+bonn2@users.noreply.github.com>
2023-11-19 11:42:55 -0500

    Project Setup

--- src/main/java/net/bonn2/Main.java
+++ src/main/java/net/bonn2/Main.java
@@ -8,6 +8,11 @@ import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 public class Main extends Module {
 
     @Override
+    public void registerLoggingChannels() {
+
+    }
+
+    @Override
     public void registerSettings() {
         Settings.register(this, "example", Setting.Type.STRING, "",
                 "This is an example setting, that is different for every server.");
